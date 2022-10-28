4094842500c975afdee3e3dfb8820c42ef99d7bd
henrychow <henry517c@gmail.com>
2022-10-28 16:48:39 +0100

    Added step 8 question 10 solution

---
+++ step-8-exercises/step_8_q10.rb
@@ -0,0 +1,36 @@
+# 10. We've been using the chomp() method to get rid of the last return character.
+# Find another method among those provided by the String class that could be used for the same purpose
+# (although it will require passing some arguments).
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.gsub(/\n/, "")
+  while !name.empty?
+    students << { name: name, cohort: :november }
+    puts "Now we have #{students.count} students"
+    name = gets.gsub(/\n/, "")
+  end
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
