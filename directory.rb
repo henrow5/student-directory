0861d7f15e4a504788c09add0f601b3a4623674b
henrychow <henry517c@gmail.com>
2022-10-29 19:31:09 +0100

    Added an interactive menu

--- directory.rb
+++ directory.rb
@@ -1,16 +1,27 @@
-# students = [
-#   { name: "Dr. Hannibal Lecter", cohort: :november },
-#   { name: "Darth Vader", cohort: :november },
-#   { name: "Nurse Ratched", cohort: :november },
-#   { name: "Michael Corleone", cohort: :november },
-#   { name: "Alex DeLarge", cohort: :november },
-#   { name: "The Wicked Witch of the West", cohort: :november },
-#   { name: "Terminator", cohort: :november },
-#   { name: "Freddy Krueger", cohort: :november },
-#   { name: "The Joker", cohort: :november },
-#   { name: "Joffrey Baratheon", cohort: :november },
-#   { name: "Norman Bates", cohort: :november },
-# ]
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
 
 def input_students
   puts "Please enter the names of the students"
@@ -46,8 +57,9 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
-students = input_students
-#nothing happens until we call the methods
-print_header
-print(students)
-print_footer(students)
+interactive_menu
+# students = input_students
+# #nothing happens until we call the methods
+# print_header
+# print(students)
+# print_footer(students)
